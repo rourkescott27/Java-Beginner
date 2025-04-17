9295ebb988db188f7b59c9bc683a38a80f5e29e3
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-04-17 09:23:59 +0200

    Chapter 3 - Minor changes

--- chapter_3/src/ex01_ch3/ShoppingCart.java
+++ chapter_3/src/ex01_ch3/ShoppingCart.java
@@ -1,6 +1,8 @@
 package ex01_ch3;
 
+// EXERCISE 3.1 - Creating the ShoppingCart class
 public class ShoppingCart {
+//    EXERCISE 3.2 - Creating the main method
     public static void main (String[] args){
         System.out.println("Welcome to the Shopping Cart!");
     }
