bb2303c911d8bee383e62eb5a371eff13efdd9a4
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-04-17 09:25:53 +0200

    Chapter 5 - Examples

---
+++ chapter_5/src/classWork/ClassWork.java
@@ -0,0 +1,52 @@
+package classWork;
+
+public class ClassWork {
+    public static void main(String[] args) {
+        /**int attendees = 5;
+        boolean largeVenue;
+
+        if(attendees >= 5){
+            largeVenue = true;
+            System.out.println("Large venue needed!");
+        }
+        else{
+            largeVenue = false;
+            System.out.println("Large venue not needed!");
+        }
+
+        largeVenue = (attendees > 5);**/
+
+        /**String itemDesc1 = "Shirt";
+        String itemDesc2 = "Trousers";
+        String itemDesc3 = "Scarf";
+
+        String[] items = new String[] {"Shirt", "Trousers", "Scarf"};
+
+        for (String item : items) {
+            System.out.println(item);
+        };**/
+
+        /**String[] names = new String[] {"George","Jill", "Xinyi", "Ravi"};
+
+        for (String name : names) {
+            System.out.println("Name is " + name);
+        }**/
+
+        /**int passmark = 12;
+        boolean passed = false;
+        int[] scores = {4,6,2,8,12,35,9};
+
+        for(int unitScore: scores) {
+            if (unitScore >= 12){
+                passed = true;
+                break;
+            }
+        }
+        System.out.println("At least one passed? " + passed);**/
+
+    }
+}
+
+
+
+
