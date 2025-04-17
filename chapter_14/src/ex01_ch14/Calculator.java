6b4db67192aca3fd6a6d2ffde6690506b35fff48
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-04-17 09:30:25 +0200

    Chapter 14 - Exercises

---
+++ chapter_14/src/ex01_ch14/Calculator.java
@@ -0,0 +1,11 @@
+package ex01_ch14;
+
+public class Calculator {
+    public int add ( int x, int y ) {
+        return x + y;
+    }
+
+    public double divide ( int x, int y ) throws ArithmeticException {
+        return x / y;
+    }
+}
