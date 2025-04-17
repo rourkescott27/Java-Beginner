e87553cd2aca18c31ef38a935cd8e8830302d952
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-04-17 09:25:20 +0200

    Chapter 4 - Minor changes

--- chapter_4/src/ex02_ch4/ShoppingCart.java
+++ chapter_4/src/ex02_ch4/ShoppingCart.java
@@ -8,19 +8,15 @@ public class ShoppingCart
         String itemDesc = "BMW G80's";
         String message = custName + " would love to buy " + itemDesc + " !";
 
-        // Declare and initialize numeric fields: price, tax, quantity, and total
-        // Do not initialize total.
         double price = 1_999_900.00;
         double tax = 1.14;
         int quantity = 2;
         double total;
 
-        // Modify message to include quantity.
         message = custName + " would love to buy " + quantity + " " + itemDesc + " !";
 
         System.out.println(message);
 
-        // Calculate total and then print the total cost.
         total = quantity * price * tax;
 
         String totalMsg = "The total price of your purchase is R" + total;
